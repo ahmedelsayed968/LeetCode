4a3c66ec009f4a0a2d87e9e4004b1d96c0aa5918
Ahmed Elsayed <88388782+ahmedelsayed968@users.noreply.github.com>
2022-06-04 17:23:52 +0200

    Time: 222 ms (7.82%), Space: 19 MB (98.03%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.cpp
+++ 283-move-zeroes/283-move-zeroes.cpp
@@ -1,19 +1,25 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        vector<int> ret;
-        vector<int> zeroes;
-        for(auto it:nums)
-        {
-            if(it != 0) ret.push_back(it);
-            else zeroes.push_back(it);
+        int check = false;
+        int size = nums.size();
+        for(int i = 0;i < size;i++){
+            if(nums[i] == 0){
+                for(int j = i+1;j < size ;j++){
+                    if(nums[j]!=0)
+                    {
+                        swap(nums[i],nums[j]);
+                        if(j==size-1) {
+                            check = true;
+                            break;
+                        }
+                        break;
+                    }
+                    
+                }
+            }
+            if(check) break;
         }
         
-        if(!zeroes.size()) nums = ret;
-        else{
-            for(auto it:zeroes)
-                ret.push_back(it);
-            nums =ret;
-        }
     }
 };
