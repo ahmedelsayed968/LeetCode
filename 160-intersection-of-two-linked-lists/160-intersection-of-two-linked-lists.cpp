9aac33c9bb3dcfaf1f6ff25a1b69003b140ec023
Ahmed Elsayed <88388782+ahmedelsayed968@users.noreply.github.com>
2022-07-19 17:23:23 +0200

    Time: 48 ms (91.13%), Space: 14.4 MB (88.34%) - LeetHub

--- 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.cpp
+++ 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.cpp
@@ -9,6 +9,12 @@
 class Solution {
 public:
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        
+        
+        return approach_2(headA, headB);
+    }
+    
+    ListNode * approach_1(ListNode *headA, ListNode *headB){
         map<ListNode*,int> freq;
         ListNode* temp = headA;
         while(temp) freq[temp]++,temp = temp->next;
@@ -21,7 +27,37 @@ public:
             temp2 = temp2->next;
         }
         return NULL;
-        
+    }
+    ListNode * approach_2(ListNode *headA, ListNode *headB){
+        // they will intersect on 3 cases only
+        // 1) skipped values are equal then -> ptr1 == ptr2 at some point
+        // 2) skipped values of 1 < skipped 2 then ptr1 == ptr2->next at some point
+        // 3) skipped values of 1 > skipped 2 then ptr2 == ptr1->next at some point
+        ListNode *ptr1 = headA;
+        ListNode *ptr2 =headB;
+        while(ptr1 != ptr2){
+            if(ptr1 !=NULL && ptr2 !=NULL){
+                if(ptr1 == ptr2->next)
+                    return ptr1;
+                else if(ptr2 == ptr1->next)
+                    return ptr2;
+                
+            }
+            if(!ptr1){
+                ptr1 = headB;
+            }
+            else
+                ptr1 = ptr1->next;
+            if(!ptr2)
+                ptr2 = headA;    
+            else
+                ptr2 = ptr2->next;
+            
+            
+            
+        }
+        return ptr1;
         
     }
+    
 };
