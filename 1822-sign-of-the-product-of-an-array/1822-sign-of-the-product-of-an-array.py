26c85a1cb219f2334c4bf962b3515cc6df996c8c
Ahmed Elsayed <88388782+ahmedelsayed968@users.noreply.github.com>
2022-08-07 19:13:20 +0200

    Time: 78 ms (72.86%), Space: 14 MB (36.07%) - LeetHub

--- 1822-sign-of-the-product-of-an-array/1822-sign-of-the-product-of-an-array.py
+++ 1822-sign-of-the-product-of-an-array/1822-sign-of-the-product-of-an-array.py
@@ -5,6 +5,8 @@ class Solution:
         # iterate until list[i] > 0
         # if zero flage raised then return 0
         # count the number of negative numbers if even return 1 else -1
+        if(0 in nums):
+            return 0
         arr = deepcopy(nums)
         arr.sort()
         zero_flage = False
@@ -12,15 +14,9 @@ class Solution:
         for i in arr:
             if(i > 0):
                 break
-            elif(i==0):
-                zero_flage  = True
             elif i < 0:
                 neg +=1
-        if(zero_flage == True):
-            return 0
-            
-            
-      
+        
         return 1 if neg%2 == 0 else -1
                 
         
