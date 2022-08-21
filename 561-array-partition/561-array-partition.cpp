963815131b24a6993f3d0b5d58b3057f9036102c
Ahmed Elsayed <88388782+ahmedelsayed968@users.noreply.github.com>
2022-08-21 19:17:59 +0200

    Time: 99 ms (22.32%), Space: 28.3 MB (47.98%) - LeetHub

--- 561-array-partition/561-array-partition.cpp
+++ 561-array-partition/561-array-partition.cpp
@@ -4,7 +4,7 @@ public:
         sort(nums.begin(),nums.end());
         int size = nums.size();
         int sum = 0;
-        for(int i = 0;i<size-1;i+=2){
+        for(int i = 0;i<size;i+=2){
             sum +=nums[i]; 
         }
         return sum;
