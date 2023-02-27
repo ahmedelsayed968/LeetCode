8f07fac135dcd6ad0af030d1f1c25147f67dc67e
Ahmed Elsayed <88388782+ahmedelsayed968@users.noreply.github.com>
2023-02-27 12:15:55 +0200

    Time: 1181 ms (10.41%), Space: 0B (100.00%) - LeetHub

--- 0608-tree-node/0608-tree-node.sql
+++ 0608-tree-node/0608-tree-node.sql
@@ -1,10 +1,27 @@
 # Write your MySQL query statement below
 
-SELECT id ,
-    CASE 
-    WHEN p_id IS NUll THEN 'Root'
-    WHEN p_id IN (SELECT id FROM Tree) AND id  IN(SELECT p_id FROM Tree) THEN 'Inner'
-    ELSE 'Leaf'
-    END 
-    AS 'Type'
-FROM Tree;
+# SELECT id ,
+#     CASE 
+#     WHEN p_id IS NUll THEN 'Root'
+#     WHEN p_id IN (SELECT id FROM Tree) AND id  IN(SELECT p_id FROM Tree) THEN 'Inner'
+#     ELSE 'Leaf'
+#     END 
+#     AS 'Type'
+# FROM Tree;
+
+SELECT id , 'Root' AS type
+FROM Tree
+WHERE p_id IS NULL
+
+UNION
+SELECT id , 'Inner' AS type
+FROM Tree
+WHERE p_id IS NOT NULL AND id IN (SELECT p_id FROM Tree)
+
+UNION
+SELECT id , 'Leaf' AS type
+FROM Tree
+WHERE p_id IS NOT NULL AND (id NOT IN (SELECT DISTINCT p_id FROM Tree WHERE p_id IS NOT NULL))
+ORDER BY id ASC;
+
+
