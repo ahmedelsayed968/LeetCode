1f8f392a03c3925960f3308c3a9c2565a9dc34bb
Ahmed Elsayed <88388782+ahmedelsayed968@users.noreply.github.com>
2023-03-03 06:17:00 +0200

    Time: 33 ms (71.06%), Space: 14.1 MB (59.65%) - LeetHub

--- 0690-employee-importance/0690-employee-importance.cpp
+++ 0690-employee-importance/0690-employee-importance.cpp
@@ -12,18 +12,35 @@ class Solution {
     map<int,vector<int>>Graph;
     map<int,int> importance;
     vector<bool> visited;
+    
+    
     int sum = 0;
+    map<int,Employee*> Graph_2;
 public:
     int getImportance(vector<Employee*> employees, int id) {
         // initialize the graph with the id--> children
         // initialize a hash table with table[id] = importance
         // dfs from the given id 
         // accumulate the importance using the hash table
-        initializeTheGraph(employees);
-        DFS(id);
+        for(auto emp:employees){
+            Graph_2[emp->id] = emp;
+        }
+        DFS_2(id);
         return sum;
         
     }
+    
+    
+    
+    
+    
+    void DFS_2(int id){
+        this->sum += Graph_2[id]->importance;
+        for(auto child:Graph_2[id]->subordinates){
+                DFS_2(child);
+        }
+    }
+    
     void DFS(int id){
         this->sum += importance[id];
         for(auto child:Graph[id]){
