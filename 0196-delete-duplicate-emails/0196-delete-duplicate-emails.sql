69c5443dcf881e9efd77dbd5dbce213ced625f23
Ahmed Elsayed <88388782+ahmedelsayed968@users.noreply.github.com>
2023-07-31 01:31:33 +0300

    Time: 1615 ms (62.88%), Space: 0B (100.00%) - LeetHub

--- 0196-delete-duplicate-emails/0196-delete-duplicate-emails.sql
+++ 0196-delete-duplicate-emails/0196-delete-duplicate-emails.sql
@@ -1,8 +1,9 @@
-/* 
- Please write a DELETE statement and DO NOT write a SELECT statement.
- Write your T-SQL query statement below
- */
+# Please write a DELETE statement and DO NOT write a SELECT statement.
+# Write your MySQL query statement below
 
-DELETE person1
-FROM Person person1 , Person person2
-WHERE person1.email = person2.email AND person1.id > person2.id;
+DELETE p1
+FROM Person AS p1
+INNER JOIN Person AS p2 
+ON p1.email = p2.email AND
+    p1.id <> p2.id AND
+    p1.id > p2.id;
